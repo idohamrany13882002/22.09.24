743e0e4426036205ebcb8bb1fe61ae04d853aead
idohamrany <idohamrany13882002@gmail.com>
2024-09-22 21:07:12 +0300

    first upload

--- exc2.py
+++ exc2.py
@@ -1,4 +1,13 @@
 # START
-# create an empty list, run from 1 to 10 append random choice
+# create an empty list, run from 1 to 10 append random choice [1, 4, 9, -2]
 # print mean
+
+import statistics
+import random
+
+x: list [int] = []
+for _ in range (11):
+    x.append(random.choice([1, 4, 9, -2]))
+print(x)
+print(statistics.mean(x))
 # END
